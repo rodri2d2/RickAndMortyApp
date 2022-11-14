5696c34e857c99df31a1796a4336663153293e98
Rodrigo Candido <rodrigoipu@hotmail.com>
2022-11-14 17:12:53 +0100

    Initial Commit
    Create blanc app to work with

---
+++ RickAndMortyApp/RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Rodrigo Candido on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
