157f5d459fc144d25c9277e49b58db7b6dc22094
Rodrigo Candido <rodrigoipu@hotmail.com>
2022-11-15 17:22:17 +0100

    Add functionality to NetworkMangers
    Implement funcionality to execute function

---
+++ RickAndMortyApp/RickAndMortyApp/Network/Services/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  RickAndMortyApp
+//
+//  Created by Rodrigo Candido on 14/11/22.
+//
+
+import Foundation
+import Combine
+
+
+class NetworkManager: Network {
+    
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func execute<T>(for request: T) -> AnyPublisher<T.Response, Error> where T : URLRequestBuilder {
+        let request = request.request()
+        return URLSession.shared
+            .dataTaskPublisher(for: request)
+            .map { $0.data }
+            .decode(type: T.Response.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
